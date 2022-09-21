fb84feb1769a7a210201d7936d6c51e1507f4d82
Brandon Rodriguez <chilebr99@gmail.com>
2022-09-20 23:34:23 -0400

    Function choices 1 of the the 3 strings in the array

---
+++ app.js
@@ -0,0 +1,7 @@
+function getcomputerchoice() {
+  const answerChoices = ["Rock", "Paper", "Scissors"];
+    const randomChoice = Math.floor(Math.random() * 3)
+    console.log(answerChoices[randomChoice])
+    
+}
+getcomputerchoice()
