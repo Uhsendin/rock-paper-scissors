0621b347c60dac8009fa49b3868b0e7c93c84cb0
Brandon Rodriguez <chilebr99@gmail.com>
2022-09-21 13:03:02 -0400

    Created playRound function

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 function getcomputerchoice() {
   const answerChoices = ["Rock", "Paper", "Scissors"];
-    const randomChoice = Math.floor(Math.random() * 3)
-    console.log(answerChoices[randomChoice])
-    
+  const randomChoice = Math.floor(Math.random() * 3);
+  const compChoice = answerChoices[randomChoice];
+  return compChoice;
 }
-getcomputerchoice()
+// console.log(getcomputerchoice())
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "Rock" && computerSelection === "Rock") {
+    return "You tied! You both picked Rock";
+  } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+    return "You tied! You both picked Paper";
+  } else if (
+    playerSelection === "Scissors" &&
+    computerSelection === "Scissors"
+  ) {
+    return "You tied! You both picked Scissors";
+  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    return "You win! Rock beats Scissors";
+  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    return "You lose! Paper beats Rock";
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    return "You win! Paper beats Rock";
+  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    return "You lose! Scissors beats Paper";
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    return "You win! Scissors beats Paper";
+  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    return "You lose! Rock beats Scissors";
+  }
+}
